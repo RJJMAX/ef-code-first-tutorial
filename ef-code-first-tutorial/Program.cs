18bb95ca0e62cfa630687699fc052847b0cac9fa
Rob Jaindl <robmax2023@outlook.com>
2023-02-27 16:05:08 -0500

    Add project files.

---
+++ ef-code-first-tutorial/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using ef_code_first_tutorial;
+using Microsoft.EntityFrameworkCore;
+
+var _context = new SalesDBContext();
+
+//var order = _context.Orders.Include(x => x.OrderLines).Where(x => x.Id == 2).ToList();
+var order = _context.Orders
+                    .Include(x => x.OrderLines) //.Include - fill in these instances
+                        .ThenInclude(x => x.Item) //fill in the instances of Item as Foreign key in OrderLines
+                    .Include(x => x.Customer)     //fill in the orderlines when i read the order
+                    .Single(x => x.Id == 2);
+
+
+Console.WriteLine($"ORDER Description:  {order.Description}");
+
+foreach(var ol in order.OrderLines) {
+    Console.WriteLine($" ORDERLINE Product: {ol.Item.Name}, Quantity:   {ol.Quantity}, " +
+        $"Price:   {ol.Item.Price:C}, Line Total:  {ol.Quantity * ol.Item.Price:C}");
+
+}
+var orderTotal = order.OrderLines.Sum(ol => ol.Item.Price * ol.Quantity);
+Console.WriteLine($"Total:  {orderTotal:C}");
+
+//_context.Customers.ToList().ForEach(c => Console.WriteLine(c.Name));
+
---
+++ ef-code-first-tutorial/SalesDBContext.cs
@@ -0,0 +1,29 @@
+﻿using ef_code_first_tutorial.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ef_code_first_tutorial; 
+public class SalesDBContext : DbContext {
+    // Tables accessible
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Item> Items { get; set; }
+    public DbSet<OrderLine> OrderLines { get; set; }
+
+    public SalesDBContext() { }
+    // from Microsoft library and passed to the parent class
+    public SalesDBContext(DbContextOptions<SalesDBContext> options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        var connStr = "server=localhost\\sqlexpress;" +
+            "database=SalesDB2;" +
+            "trusted_connection=true;" +
+            "trustServerCertificate=true;";
+        optionsBuilder.UseSqlServer(connStr);
+    }
+
+}
